6e3d51c7f2578bc7ebfc38339722f924cfa73f18
hungdt1k3 <58964960+hungdt1k3@users.noreply.github.com>
2019-12-18 21:39:14 +0630

    chuong trinh
    do nhiet do

---
+++ chuong trinh.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "stm32l0xx_hal.h"
+#include "lcd_4bit.h"
+extern uint16_t nhiet_do1,nhiet_do2;
+void hien_thi(void)
+{
+		lcd_gotoxy(0,2);
+		lcd_putsf("MD1:");	
+		lcd_putchar((nhiet_do1/10)+0x30);
+		lcd_putchar((nhiet_do1%10)+0x30);	
+
+		lcd_gotoxy(10,2);
+		lcd_putsf("MD2:");	
+		lcd_putchar((nhiet_do2/10)+0x30);
+		lcd_putchar((nhiet_do2%10)+0x30);
+
+		lcd_gotoxy(0,3);
+		lcd_putsf("MD3:");	
+		lcd_putchar((nhiet_do2/10)+0x30);
+		lcd_putchar((nhiet_do2%10)+0x30);
+
+
+		lcd_gotoxy(10,3);
+		lcd_putsf("MD3:");	
+		lcd_putchar((nhiet_do2/10)+0x30);
+		lcd_putchar((nhiet_do2%10)+0x30);
+
+
+}	
